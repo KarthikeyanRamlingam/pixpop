742401fc9ff2ef4c5545573af3b706489c10272b
karthik <karthikeyanramalingam549@gmail.com>
2025-10-06 20:01:37 +0530

    Initial commit of Pixpop project

--- src/components/loader.js
+++ src/components/loader.js
@@ -3,9 +3,15 @@ import { TailSpin } from "react-loader-spinner";
 
 const Loader = () => {
   return (
-    <div style={{ marginTop: "20px" }}>
-      <TailSpin color="#00BFFF" height={80} width={80} />
-      <p style={{ color: "#ccc" }}>Generating image...</p>
+    <div style={{ marginTop: "20px", textAlign: "center" }}>
+      <TailSpin
+        visible={true}
+        height={80}
+        width={80}
+        color="#00BFFF"
+        ariaLabel="tail-spin-loading"
+      />
+      <p style={{ color: "#ccc", marginTop: 10 }}>Generating image...</p>
     </div>
   );
 };
